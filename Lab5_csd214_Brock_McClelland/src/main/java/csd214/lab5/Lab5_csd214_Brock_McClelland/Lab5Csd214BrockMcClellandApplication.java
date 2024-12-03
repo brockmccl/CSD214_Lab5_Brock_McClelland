c53250580560900b0cd5c194d5e8411375f899fe
Brock McClelland <23034226@saultcollege.ca>
2024-12-03 16:00:59 -0500

    added content from lab

---
+++ Lab5_csd214_Brock_McClelland/src/main/java/csd214/lab5/Lab5_csd214_Brock_McClelland/Lab5Csd214BrockMcClellandApplication.java
@@ -0,0 +1,13 @@
+package csd214.lab5.Lab5_csd214_Brock_McClelland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5Csd214BrockMcClellandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5Csd214BrockMcClellandApplication.class, args);
+	}
+
+}
---
+++ Lab5_csd214_Brock_McClelland/src/test/java/csd214/lab5/Lab5_csd214_Brock_McClelland/Lab5Csd214BrockMcClellandApplicationTests.java
@@ -0,0 +1,13 @@
+package csd214.lab5.Lab5_csd214_Brock_McClelland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5Csd214BrockMcClellandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
